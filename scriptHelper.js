3d640562680184b5d2489060928106dd0deb08ae
Ryan Murphy <ryanmurphy@RPM-MBP.local>
2023-03-12 17:30:43 -0500

    several updates but problem with lots of node files being deleted or created

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,18 +2,18 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    let destination = document.getElementById("missionTarget");
+    destination.innerHTML = `
+        <h2>Mission Destination</h2>
+        <ol>
+            <li>Name: ${name}</li>
+            <li>Diameter: ${diameter}</li>
+            <li>Star: ${star}</li>
+            <li>Distance from Earth: ${distance}</li>
+            <li>Number of Moons: ${moons}</li>
+        </ol>
+        <img src="${imageUrl}">
+        `;
 }
 
 function validateInput(testInput) {
@@ -46,32 +46,33 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
         copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
         let launchStatus = document.getElementById("launchStatus");
-        if (fuelLevel < 10000 && cargoLevel > 10000) {
-            fuelStatus.innerHTML = "The fuel level is too low for launch";
-            cargoStatus.innerHTML = "The weight of the cargo is too heavy for launch"
+        if (fuelLevel < 10000 && cargoLevel <= 10000) {
+            fuelStatus.innerHTML = "Fuel level too low for launch";
+            cargoStatus.innerHTML = "Cargo mass low enough for launch"
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-            launchStatus.style.color = red;
+            launchStatus.style.color = "#C7254E";
         } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
-            fuelStatus.innerHTML = "The fuel level is high enough for launch"
-            cargoStatus.innerHTML = "The weight of the cargo is too heavy for launch";
+            fuelStatus.innerHTML = "Fuel level high enough for launch"
+            cargoStatus.innerHTML = "Cargo mass too heavy for launch";
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-            launchStatus.style.color = red;
-        } else if (fuelLevel < 10000 && cargoLevel <= 10000) {
-            fuelStatus.innerHTML = "The fuel level is too low for launch";
-            cargoStatus.innerHTML = "The weight of the cargo is OK for launch";
+            launchStatus.style.color = "#C7254E";
+        } else if (fuelLevel < 10000 && cargoLevel > 10000) {
+            fuelStatus.innerHTML = "Fuel level too low for launch";
+            cargoStatus.innerHTML = "Cargo mass too heavy for launch";
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-            launchStatus.style.color = red;
+            launchStatus.style.color = "#C7254E";
         } else {
-            fuelStatus.innerHTML = "The fuel level is high enough for launch";
-            cargoStatus.innerHTML = "The weight of the cargo is OK for launch";
+            fuelStatus.innerHTML = "Fuel level high enough for launch"
+            cargoStatus.innerHTML = "Cargo mass low enough for launch"
             launchStatus.innerHTML = "Shuttle is Ready for Launch";
-            launchStatus.style.color = green;
+            launchStatus.style.color = "#419F6A";
         }
     }
  }
 
 async function myFetch() {
-    let planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+    let planetsReturned;
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
         return response.json();
     });
     return planetsReturned;
